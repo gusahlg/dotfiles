121b4e5493f548d7dd549111246228bc2fe5992a
gusahlg <gusahlg@gmail.com>
2026-02-12 17:55:38 +0100

    Updated river setup plus cleanup

--- private_dot_config/river/executable_miner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-
-sudo nvidia-smi -pl 165
-cd ~/miners/trex
-./t-rex -a kawpow -o stratum+tcp://rvn.2miners.com:6060 \
--u RTNzb97f2pSZYJCG5xTW4FJKZrb4ftpnpR.worker1 -i 18
-
-exit 0
